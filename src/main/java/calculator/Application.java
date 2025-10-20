1c65abdfb460efd8e75c91f946a394e322add5e6
leeminho <tommymh97@naver.com>
2025-10-20 23:13:56 +0900

    feat(main) : 콘솔 입력을 받아 계산을 수행하고 결과를 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+
 public class Application {
+    private static final String DEFAULT_DELIMITERS_REGEX = "[,:]";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String NEWLINE_TOKEN = "\\n";
+    private static final int INDEX_NOT_FOUND = -1;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.print("입력하세요: ");
+        String input = Console.readLine();
+
+        int sum = calculateSum(input);
+        System.out.println("결과 : " + sum);
     }
 }
